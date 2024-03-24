5c5d4243a53be91604299761393a3735a0b87f3d
GIL ALBA, EDUARDO <eduardo.gil.alba@alumnos.upm.es>
2024-03-24 01:34:56 +0100

    Primer intento

--- Practica 1/code.pl
+++ Practica 1/code.pl
@@ -1,6 +1,6 @@
 :- module(_,_,[assertions,regtypes]).
 % :- module(_,_,[]).           % For pure LP, depth-first search rule
-:- module(_,_,['sr/bfall']).   % For pure LP, breadth-first search rule, all predicates
+%:- module(_,_,['sr/bfall']).   % For pure LP, breadth-first search rule, all predicates
 
 author_data('Gil', 'Alba', 'Eduardo', 'Z170238').
 
@@ -9,3 +9,17 @@ author_data('Gil', 'Alba', 'Eduardo', 'Z170238').
 %                                                   PRACTICA 1                                                                     %
 %                                                                                                                                  %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+charge( +++++++ ).
+charge( ++++++ ).
+charge( +++++ ).
+charge( ++++ ).
+charge( +++ ).
+charge( ++ ).
+charge( + ).
+charge( 0 ).
+
+my_list([]).
+my_list([_|T]) :- my_list(T).
+
+basic_surface([H|T]) :- my_list([H]), my_list([T]). 
