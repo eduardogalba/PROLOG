14191720ab8e643b6ee1cb2bab7874eea9bf7c0c
GIL ALBA, EDUARDO <eduardo.gil.alba@alumnos.upm.es>
2024-03-25 17:36:46 +0100

    surface\1 funciona!

--- Practica 1/code.pl
+++ Practica 1/code.pl
@@ -14,14 +14,6 @@ author_data('Gil', 'Alba', 'Eduardo', 'Z170238').
 :- doc(author, "Eduardo Gil Alba, z170238").
 
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%                                                                                                                                  %
-%                                                   PRACTICA 1                                                                     %
-%                                                                                                                                  %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
 charge( +++++++ ).
 charge( ++++++ ).
 charge( +++++ ).
@@ -43,7 +35,9 @@ my_list([+]).
 my_list([0]).
 my_list([]).
 
-my_list([H|T]) :- charge(H), my_list(T).
+my_list([H|T]) :- 
+   charge(H), 
+   my_list(T).
 
 :- doc(basic_surface/1,"Defines a surface of charged cells and it´s represented by a list of lists. It is defined as: @includedef{basic_surface/1}\n
 @begin{alert}
@@ -53,13 +47,35 @@ my_list([H|T]) :- charge(H), my_list(T).
 #"@var{CellList} is a list with charged cells.".
 
 
-:- test prop basic_surface(X)   
+:- test basic_surface(X)   
    : (X = [[]])  
    => fails
    # "Cannot be a list with a empty sublist".
 
 
-basic_surface([[H|T]]) :-  charge(H), my_list(T).
-basic_surface([[H|T]|T2]) :- charge(H), my_list(T), basic_surface(T2).
+basic_surface([[H|T]]) :-  
+   charge(H), 
+   my_list(T).
 
+basic_surface([[H|T]|T2]) :- 
+   charge(H), 
+   my_list(T), 
+   basic_surface(T2).
+
+mylength([],0).
+mylength([_|T],s(N)) :-
+    mylength(T,N).
+
+
+surface([L|L2]) :- 
+   basic_surface([L|L2]), 
+   mylength(L, Tam), 
+   surface_acc([L|L2], Tam).
+
+surface_acc([],_).
+surface_acc([[H|T]|T2], Acc) :- 
+   charge(H), my_list(T), 
+   mylength([H|T],NewAcc), 
+   Acc == NewAcc, 
+   surface_acc(T2,NewAcc).
 
