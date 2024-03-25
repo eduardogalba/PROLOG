de24a2826091e82d8acf7518966f05f1a4442dc3
GIL ALBA, EDUARDO <eduardo.gil.alba@alumnos.upm.es>
2024-03-25 21:49:05 +0100

    h_line/3 y v_line/3 en bucle infinito

--- Practica 1/code.pl
+++ Practica 1/code.pl
@@ -14,6 +14,8 @@ author_data('Gil', 'Alba', 'Eduardo', 'Z170238').
 :- doc(author, "Eduardo Gil Alba, z170238").
 
 
+:- doc(charge/1,"Defines possible constant values of my cell charges. 
+It is defined as: @includedef{charge/1}\n").
 charge( +++++++ ).
 charge( ++++++ ).
 charge( +++++ ).
@@ -23,7 +25,12 @@ charge( ++ ).
 charge( + ).
 charge( 0 ).
 
+:- doc(my_list/1,"Defines a list with [Head|Tail] structure and cell charges as elements. 
+It is defined as: @includedef{my_list/1}\n").
 
+my_list([H|T]) :- 
+   charge(H), 
+   my_list(T).
 
 my_list([+++++++]).
 my_list([++++++]).
@@ -35,17 +42,21 @@ my_list([+]).
 my_list([0]).
 my_list([]).
 
-my_list([H|T]) :- 
-   charge(H), 
-   my_list(T).
 
-:- doc(basic_surface/1,"Defines a surface of charged cells and it´s represented by a list of lists. It is defined as: @includedef{basic_surface/1}\n
-@begin{alert}
-@bf{Note:} @tt{basic_surface/1} must have at least one sublist with a charged cell.
-@end{alert}").
+
+:- doc(basic_surface/1,"Defines a surface of charged cells represented by a list of lists. 
+It is defined as: @includedef{basic_surface/1}\n
+@begin{note}
+@bf{Note:} @pred{basic_surface/1} must have at least one sublist with a charged cell.
+@end{note}").
 :- prop basic_surface(CellList) 
 #"@var{CellList} is a list with charged cells.".
+:- doc(basic_surface/1, "
+   @section{Other properties}
+   @subsection{Test}
+   @em{It will be introduced bla bla}
 
+").
 
 :- test basic_surface(X)   
    : (X = [[]])  
@@ -67,6 +78,11 @@ mylength([_|T],s(N)) :-
     mylength(T,N).
 
 
+:- doc(surface/1,"Defines a surface of charged cells, represented by a list of lists with same number of cells. 
+It is defined as: @includedef{surface/1}\n").
+:- prop surface(CellList) 
+#"@var{CellList} is a list with charged cells.".
+
 surface([L|L2]) :- 
    basic_surface([L|L2]), 
    mylength(L, Tam), 
@@ -79,3 +95,31 @@ surface_acc([[H|T]|T2], Acc) :-
    Acc == NewAcc, 
    surface_acc(T2,NewAcc).
 
+get([Elem|_], s(0), Elem).
+get([_|Rest], s(Index), Elem) :- 
+   get(Rest, Index, Elem).
+
+h_line([L|L2], N, C) :-
+   surface([L|L2]),
+   get([L|L2], N, C).
+   
+
+
+v_line([L|L2], N, C) :-
+   surface([L|L2]),
+   v_line_aux([L|L2], N, C).
+   
+
+myappend([],L,L) :- 
+   list(L).
+myappend([X|Xs],Ys,[X|Zs]) :- 
+   myappend(Xs,Ys,Zs).
+
+v_line_aux(_,_,[]).
+v_line_aux([L|L2], N, [E|T]) :-
+   get(L, N, E),
+   v_line_aux(L2, N, T).
+
+
+total_charge([L|[L2|T]], ) :- 
+   myappend(L,L2, Z),
